28732b69fd71a15d1bc8ff967593ba47412f367c
Brittany Russell <sincerelybr@gmail.com>
2020-03-02 01:08:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,15 @@
+def turn_count
 
+apples_in_basket = basket.size # Step 1
+apples_taken_out = 0 # Step 2
+
+loop do # Step 3
+    if apples_taken_out < apples_in_basket 
+        # Step 4
+        puts "Taking out #{basket[apples_taken_out]}"
+        apples_taken_out += 1
+    else
+        # Step 5
+        break
+    end
+end
