6ebeda574bacfb46cec9197ae15d2f7c71473853
Brittany Russell <sincerelybr@gmail.com>
2020-03-02 01:10:40 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,13 +3,8 @@ def turn_count(board)
 board_in_basket = board.size # Step 1
 board_taken_out = 0 # Step 2
 
-loop do # Step 3
-    if board_taken_out < board_in_basket 
-        # Step 4
-        puts "Taking out #{board[board_taken_out]}"
-        board_taken_out += 1
-    else
-        # Step 5
-        break
-    end
+counter = 0
+if my_condition_is_met
+  counter += 1
+end
 end
