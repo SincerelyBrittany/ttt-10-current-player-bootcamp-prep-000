4ce2770184cfba8c852ce0e04e8597d17e231ebe
Brittany Russell <sincerelybr@gmail.com>
2020-03-02 01:07:46 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,4 @@
-
+Learn more or give us feedback
 def turn_count(board)
   # board.count{|token| token == "X" || token == "O"}
 
@@ -15,3 +15,32 @@ end
 def current_player(board)
   turn_count(board) % 2 == 0 ? "X" : "O"
 end
+
+
+
+
+# def turn_count(board)
+#   board = ["X", "O", "X", "O","X", "O","X", "O","X"]
+#   board.each do |x|
+#   puts "Primary Color #{x} is #{x.length} letters long."
+  
+#   counter = 0
+# if my_condition_is_met
+#   counter += 1
+# end
+# end
+# end
+  
+# turn_count("X")
+  
+#   primary_colors = ["Red", "Yellow", "Blue"]
+# primary_colors.each do |color|
+#   puts "Primary Color #{color} is #{color.length} letters long."
+# end
+
+
+
+def current_player()
+
+
+
